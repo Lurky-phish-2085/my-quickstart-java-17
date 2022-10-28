6373b6d5c833d37d732510e86a37b4c5cdd8d82d
lurkyphish <Lurky-phish-2085@users.noreply.github.com>
2022-10-28 16:44:39 +0800

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,20 @@
+package $package;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MainTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
