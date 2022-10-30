717ca20a27b3422e13b3c00be24527d0d9ed4ab5
lurkyphish <Lurky-phish-2085@users.noreply.github.com>
2022-10-30 23:13:15 +0800

    MainTest.java migrated from JUNIT 4 to Jupiter

--- src/main/resources/archetype-resources/src/test/java/MainTest.java
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -1,8 +1,7 @@
 package $package;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
